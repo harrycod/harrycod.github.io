5592acc93c2d1754bb93ab77d044a4117ff6022e
harrycod <whtngus1230@gmail.com>
2021-04-13 15:41:59 +0900

    first commit

---
+++ content/blog/JS/test.md
@@ -0,0 +1,9 @@
+---
+title: test
+date: 2021-04-13 13:04:26
+category: JS
+thumbnail: { thumbnailSrc }
+draft: false
+---
+
+### 테스트 페이지
---
+++ gatsby-meta-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: `harrycod's Blog`,
+  description: `Blog posted about ...`,
+  author: `harrycod`,
+  introduction: `Frontend Developer @TagbyCompany
+  배우고 익힌 것을 공유합니다.`,
+  siteUrl: `https://harrycod.github.io`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `harrycod`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: `ddu_hyun`, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: 'harrycod', // Your disqus-short-name. check disqus.com.
+    utterances: '', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'harrycod',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: 'UA-177820773-1', // Add your google analytics tranking ID
+}
